9c600f852b0341b01887a443dd67f14657562cdc
beatuano <beatuano@gmail.com>
2023-07-06 21:34:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header";
+import DarkMode from "./Components/DarkMode";
+import Virtual from "./Components/Virtual";
+import InPerson from "./Components/InPerson";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <DarkMode />
+      <div className="Main">
+        <Virtual />
+        <InPerson />
+      </div>
     </div>
   );
 }
